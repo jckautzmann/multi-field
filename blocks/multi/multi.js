66f6346d80d3ce5cd67cfdf84af293d49bb264b0
jckautzmann <jkautzma@adobe.com>
2025-09-03 12:03:26 +0200

    feat: multi
    - grouping of multi fields

--- blocks/multi/multi.js
+++ blocks/multi/multi.js
@@ -2,6 +2,7 @@ export default async function decorate(block) {
   
   
   // load the component-models.json file
+  /*
   const componentModels = await fetch(`${window.hlx.codeBasePath}/component-models.json`);
   if (componentModels.ok) {
     const data = await componentModels.json();
@@ -17,6 +18,8 @@ export default async function decorate(block) {
       row.insertBefore(div, row.firstChild);
     });
   }
+  
+   */
   
   // create block header with block options
   const div = document.createElement('div');
